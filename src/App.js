9a88fe86592b7c152d1ea026ebdb89e005cd0cc7
dixitc <dixit@triconinfotech.com>
2016-03-31 19:22:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <h1>Hello there homie, world. 	<p>Howdy , hows it going mate ?</p></h1>
+
     );
   }
 }
